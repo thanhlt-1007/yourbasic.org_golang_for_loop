1138acffc5117e30c2619350a4a7a4ce9bd19035
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-21 11:31:13 +0700

    Update main.go Add exit a loop

--- main.go
+++ main.go
@@ -4,27 +4,37 @@ import "fmt"
 
 func main() {
     // Three-component loop
-    for i := 1; i < 5; i ++ {
-        fmt.Println(i)
-    }
+    // for i := 1; i < 5; i ++ {
+    //     fmt.Println(i)
+    // }
 
     // While loop
-    n := 1
-    for n < 5 {
-        n *= 2
-        fmt.Println(n)
-    }
+    // n := 1
+    // for n < 5 {
+    //     n *= 2
+    //     fmt.Println(n)
+    // }
 
     // Infinite loop
-    sum := 0
-    for {
-        sum ++
-        break
-    }
+    // sum := 0
+    // for {
+    //     sum ++
+    //     break
+    // }
 
     // For-each range loop
-    strings := []string{"hello", "world"}
-    for i, s := range strings {
-        fmt.Println(i, s)
+    // strings := []string{"hello", "world"}
+    // for i, s := range strings {
+    //     fmt.Println(i, s)
+    // }
+
+    // Exit a loop
+    sum := 0
+    for i := 1; i < 5; i ++ {
+        if i % 2 != 0 {
+            continue
+        }
+        sum += i
     }
+    fmt.Println(sum)
 }
